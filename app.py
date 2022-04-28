091a383d3eee2f54d9a28f0fb7104832fcf22068
Ivan <ivandanylenko2003@gmail.com>
2022-04-28 20:21:02 +0300

    Структурные изменения
    Перенес импорты в отдельный файл

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout  # размещение виджетов по сетке
-from kivy.uix.stacklayout import StackLayout  # размещение виджетов сколько везет в строку
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from imports import *
+
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+
 
 class Container(BoxLayout):
-    pass
+    text_input = ObjectProperty()
+    enter_btn = ObjectProperty()
+
+    def send_data(self):
+        if self.text_input.text != '':
+            print(self.text_input.text)
+            self.text_input.text = ''
+            print(self.text_input.size)
+
+    def rescale_text_input(self):
+        pass
+
+    def rescale_enter_btn(self):
+        self.enter_btn.size[0] = self.size[1] * 0.06
 
 
 class BobbyApp(App):
---
+++ imports.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.config import Config
+from kivy.clock import Clock
+
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout  # размещение виджетов по сетке
+from kivy.uix.stacklayout import StackLayout  # размещение виджетов сколько везет в строку
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
