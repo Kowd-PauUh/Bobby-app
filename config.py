694c54f9d623656ce8f160e4800f3fcfc45789fd
Ivan <ivandanylenko2003@gmail.com>
2022-07-07 14:33:48 +0300

    Добавил конфиг файлы + апдейт гитигнора

---
+++ config.py
@@ -0,0 +1,9 @@
+
+SECTIONS = ['Diet', 'Costs', 'To-do', 'Settings']
+
+MAIN_CONFIG_PATH = 'AppData/main_config.ini'
+COSTS_CONFIG_PATH = 'AppData/costs_config.ini'
+DIET_CONFIG_PATH = 'AppData/diet_config.ini'
+TODO_CONFIG_PATH = 'AppData/todo_config.ini'
+
+COSTS_HISTORY_PATH = 'AppData/costs-history.db'
